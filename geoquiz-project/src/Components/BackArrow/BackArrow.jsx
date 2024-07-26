a6beaa25605d78cec2dbe31a7316e44e2b0d8e84
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-07-25 20:03:03 -0400

    Created backarrow functionality

---
+++ geoquiz-project/src/Components/BackArrow/BackArrow.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './BackArrow.module.css'
+
+export const BackArrow = () => {
+  return (
+    <img src="assets/backarrow.png" alt="Back arrow icon" className={styles.arrowimg}/>
+  )
+}
--- geoquiz-project/src/Pages/MenuPage/Menu.jsx
+++ geoquiz-project/src/Pages/MenuPage/Menu.jsx
@@ -1,12 +1,23 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 import styles from "./Menu.module.css"
+import { BackArrow } from '../../Components/BackArrow/BackArrow'
+import { Link } from 'react-router-dom'
 
 
 export const Menu = () => {
   return (
-    <motion.div className={styles.container}initial={{opacity:0, y:100}}
-    animate={{opacity: 1, y:0}}
-    transition={{duration: 1, ease:"easeOut", delay:0.2}}>Menu</motion.div>
+    <motion.div 
+        className={styles.container}
+        initial={{opacity:0, y:100}}
+        animate={{opacity: 1, y:0}}
+        transition={{duration: 1, ease:"easeOut", delay:0.2}}
+    >
+
+        <Link to="/">
+          <BackArrow/> 
+        </Link>
+            
+    </motion.div>
   )
 }
