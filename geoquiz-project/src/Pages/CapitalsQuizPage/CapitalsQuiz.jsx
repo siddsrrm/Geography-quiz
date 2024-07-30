c3abd8ea3c5270332f61b4909fbb0b6f1565159b
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-07-30 17:55:31 -0400

    created reactive answer choices

--- geoquiz-project/src/Pages/CapitalsQuizPage/CapitalsQuiz.jsx
+++ geoquiz-project/src/Pages/CapitalsQuizPage/CapitalsQuiz.jsx
@@ -7,10 +7,17 @@ export const CapitalsQuiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0); 
   const [answerIndex, setAnswerIndex] = useState(null);
   const [answer, setAnswer] = useState(null);
+  const [isAnswered, setIsAnswered] = useState(false);
   const { question, choices, correctAnswer } = questions[currentQuestion];
 
   const onAnswerClick = (answer, index) => {
+
+    if (isAnswered) {
+      return;
+    }
+    
     setAnswerIndex(index);
+    setIsAnswered(true);
 
     if (answer === correctAnswer) {
       setAnswer(true);
@@ -22,35 +29,34 @@ export const CapitalsQuiz = ({ questions }) => {
 
   return (
     <motion.div
-        initial={{opacity:0, y:100}}
-        animate={{opacity: 1, y:0}}
-        transition={{duration: 1, ease:"easeOut", delay:0.2}}
-        className={styles.container}
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
+      className={styles.container}
     >
-       
       <>
-          <span className={styles.questionnum}>{currentQuestion + 1}</span>
-          <span className={styles.totalquestionnum}>/{questions.length}</span>
-          <h2>{question}</h2>
-          <ul>
-            { 
-              choices.map((answer, index) =>(
-
-                <li
-                  onClick={() => onAnswerClick(answer, index)} 
-                  key={answer}
-                  className={answerIndex === index ? styles.selectedAnswer : null}
-                >
-                  {answer}
-
-                </li>
-                
-              ))
+        <span className={styles.questionnum}>{currentQuestion + 1}</span>
+        <span className={styles.totalquestionnum}>/{questions.length}</span>
+        <h2>{question}</h2>
+        <ul>
+          {choices.map((choice, index) => {
+            let className;
+            if (isAnswered) {
+              className = choice === correctAnswer ? styles.correctanswer : styles.incorrectanswer;
             }
-          </ul>
+
+            return (
+              <li
+                onClick={onAnswerClick}
+                key={choice}
+                className={className}
+              >
+                {choice}
+              </li>
+            );
+          })}
+        </ul>
       </>
-      
-    
     </motion.div>
   )
 }
