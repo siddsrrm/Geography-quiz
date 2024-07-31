13203ba54e64bc740a7c080ce2a8ee00a5cffed1
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-07-31 14:48:07 -0400

    created result page and fixed scoring bug

--- geoquiz-project/src/Pages/CapitalsQuizPage/CapitalsQuiz.jsx
+++ geoquiz-project/src/Pages/CapitalsQuizPage/CapitalsQuiz.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
-import { motion } from "framer-motion"
-import styles from "./CapitalsQuiz.module.css"
-import { useState } from 'react'
-import { resultInitialState } from '../../constants'
+import React from 'react';
+import { motion } from "framer-motion";
+import styles from "./CapitalsQuiz.module.css";
+import { useState } from 'react';
+import { resultInitialState } from '../../constants';
 
 export const CapitalsQuiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0); 
   const [answerIndex, setAnswerIndex] = useState(null);
   const [answer, setAnswer] = useState(null);
   const [isAnswered, setIsAnswered] = useState(false);
-  const[result, setResult] = useState(resultInitialState);
-  const[showResult, setShowResult] = useState(false);
+  const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
   const { question, choices, correctAnswer } = questions[currentQuestion];
 
   const onAnswerClick = (answer, index) => {
-
     if (isAnswered) {
       return;
     }
@@ -24,29 +23,29 @@ export const CapitalsQuiz = ({ questions }) => {
 
     if (answer === correctAnswer) {
       setAnswer(true);
-    }
-    else {
+      console.log("correct");
+    } else {
       setAnswer(false);
+      console.log("incorrect");
     }
-  }
+  };
 
   const onClickNext = () => {
     setAnswerIndex(null);
-    setIsAnswered(null);
+    setIsAnswered(false);
 
     setResult((prev) => ({
       ...prev,
       correctAnswers: answer ? prev.correctAnswers + 1 : prev.correctAnswers
     }));
 
-    if (currentQuestion !== questions.length - 1){
+    if (currentQuestion !== questions.length - 1) {
       setCurrentQuestion((prev) => prev + 1);
-    }
-    else {
+    } else {
       setCurrentQuestion(0);
       setShowResult(true);
     }
-  }
+  };
 
   return (
     <motion.div
@@ -55,36 +54,44 @@ export const CapitalsQuiz = ({ questions }) => {
       transition={{ duration: 1, ease: 'easeOut', delay: 0.2 }}
       className={styles.container}
     >
-      {!showResult ? (<>
-        <span className={styles.questionnum}>{currentQuestion + 1}</span>
-        <span className={styles.totalquestionnum}>/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((choice, index) => {
-            let className;
-            if (isAnswered) {
-              className = choice === correctAnswer ? styles.correctanswer : styles.incorrectanswer;
-            }
+      {!showResult ? (
+        <>
+          <span className={styles.questionnum}>{currentQuestion + 1}</span>
+          <span className={styles.totalquestionnum}>/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((choice, index) => {
+              let className;
+              if (isAnswered) {
+                className = choice === correctAnswer ? styles.correctanswer : styles.incorrectanswer;
+              }
 
-            return (
-              <li
-                onClick={onAnswerClick}
-                key={choice}
-                className={className}
-              >
-                {choice}
-              </li>
-            );
-          })}
-        </ul>
-        <div className={styles.buttonContainer}>
-          <button onClick={onClickNext} disabled={isAnswered === false} className={styles.button}>
-            {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-          </button>
+              return (
+                <li
+                  onClick={() => onAnswerClick(choice, index)}
+                  key={choice}
+                  className={className}
+                >
+                  {choice}
+                </li>
+              );
+            })}
+          </ul>
+          <div className={styles.buttonContainer}>
+            <button onClick={onClickNext} disabled={!isAnswered} className={styles.button}>
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className={styles.result}>
+          <h3>Result</h3>
+          <p>
+            You got <span>{result.correctAnswers}</span> out of <span>{questions.length}</span> questions correct!
+          </p>
+          
         </div>
-      </>) : <div className={styles.result}>
-        <h3>Result</h3>
-        </div>}
+      )}
     </motion.div>
   )
 }
