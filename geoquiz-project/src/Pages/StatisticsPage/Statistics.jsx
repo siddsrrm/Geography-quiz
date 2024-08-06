9bb184d003c6b512e1c4857bd4b99ad3c4fa8643
SIDDHARTH-HP\siddharth <siddsrrm@gmail.com>
2024-08-06 19:48:52 -0400

    added backarrow to statistics page

--- geoquiz-project/src/Pages/StatisticsPage/Statistics.jsx
+++ geoquiz-project/src/Pages/StatisticsPage/Statistics.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { BackArrow } from '../../Components/BackArrow/BackArrow'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 export const Statistics = () => {
-  return (
-    <div>Statistics</div>
-  )
+    return (
+        <motion.div 
+            initial={{opacity:0, y:100}}
+            animate={{opacity: 1, y:0}}
+            transition={{duration: 1, ease:"easeOut", delay:0.2}}
+        >
+    
+            <Link to="/Menu">
+              <BackArrow/> 
+            </Link>
+           
+        </motion.div>
+    )
 }
